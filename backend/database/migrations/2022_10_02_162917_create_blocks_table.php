8b93cefe91b993fa5b340390661ab78bf2efc67a
Moustafa <moustafaism013@hotmail.com>
2022-10-02 19:49:09 +0300

    Backend: Created a migrtion for blocks table

---
+++ backend/database/migrations/2022_10_02_162917_create_blocks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blocks', function (Blueprint $table) {
+            $table->integer('blocking_id')->references('id')->on('users');
+            $table->integer('blocked_id')->references('id')->on('users');
+            $table->timestamps();
+
+            $table->primary(['blocking_id', 'blocked_id']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blocks');
+    }
+};
