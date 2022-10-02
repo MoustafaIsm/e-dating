c0a8c1a46401e604f83fb056ace5ee696e002d0f
Moustafa <moustafaism013@hotmail.com>
2022-10-02 19:27:54 +0300

    Backend: Created a migrtion for users table

--- backend/database/migrations/2022_10_02_161651_create_users_table.php
+++ backend/database/migrations/2022_10_02_161651_create_users_table.php
@@ -13,14 +13,17 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->string("full_name");
+            $table->string("email");
+            $table->string("password");
+            $table->string("bio");
+            $table->string("location");
+            $table->integer("age");
+            $table->string("gender");
+            $table->string("intrested_in");
+            $table->string("profile_picture_url");
             $table->timestamps();
         });
     }
@@ -32,6 +35,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('users');
     }
 };
