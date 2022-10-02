87c64080fc177a6667d41cc75ae69eb7ad2c6f3f
Moustafa <moustafaism013@hotmail.com>
2022-10-02 17:00:09 +0300

    Frontend: Added the finctionality of each navigation button in the burger menu and split the javascript file into multiple

--- frontend/scripts/main_page_script.js
+++ frontend/scripts/main_page_script.js
@@ -1,25 +1,3 @@
-// Variables
-const homeNavBtn = document.getElementById("home-nav-btn");
-const msgsNavBtn = document.getElementById("msgs-nav-btn");
-const profileNavBtn = document.getElementById("profile-nav-btn");
-const homePage = document.getElementById("home-page");
-const msgsPage = document.getElementById("msgs-page");
-const profilePage = document.getElementById("profile-page");
-
-const burgerMenu = document.getElementById("burger-menu");
-const burgerMenuBtns = document.getElementById("burger-menu-btns");
-
-const editProfilemodal = document.getElementById("edit-profile-modal");
-const editProfileBtn = document.getElementById("edit-profile-btn");
-const closeEditProfileBtn = document.getElementById("close-edit-profile-btn");
-
-
-// Modal related stuff
-if (typeof editProfilemodal.showModal !== 'function') {
-    editProfilemodal.hidden = true;
-}
-
-
 // Listeners functions
 const openHomePage = () => {
     openPage("home");
@@ -53,6 +31,9 @@ const closeEditProfile = () => {
 homeNavBtn.addEventListener("click", openHomePage);
 msgsNavBtn.addEventListener("click", openMsgsPage);
 profileNavBtn.addEventListener("click", openProfilePage);
+homeBurgerNavBtn.addEventListener("click", openHomePage);
+msgsBurgerNavBtn.addEventListener("click", openMsgsPage);
+profileBurgerNavBtn.addEventListener("click", openProfilePage);
 
 burgerMenu.addEventListener("click", openMenu);
 
@@ -60,32 +41,6 @@ editProfileBtn.addEventListener("click", openEditProfile);
 closeEditProfileBtn.addEventListener("click", closeEditProfile);
 
 
-// Helper functions
-const openPage = (page) => {
-    if (page == "home") {
-        homePage.classList.remove("hide");
-        homeNavBtn.classList.add("active-nav-btn");
-        msgsPage.classList.add("hide");
-        msgsNavBtn.classList.remove("active-nav-btn");
-        profilePage.classList.add("hide");
-        profileNavBtn.classList.remove("active-nav-btn");
-    }
-    if (page == "msgs") {
-        homePage.classList.add("hide");
-        homeNavBtn.classList.remove("active-nav-btn");
-        msgsPage.classList.remove("hide");
-        msgsNavBtn.classList.add("active-nav-btn");
-        profilePage.classList.add("hide");
-        profileNavBtn.classList.remove("active-nav-btn");
-    }
-    if (page == "profile") {
-        homePage.classList.add("hide");
-        homeNavBtn.classList.remove("active-nav-btn");
-        msgsPage.classList.add("hide");
-        msgsNavBtn.classList.remove("active-nav-btn");
-        profilePage.classList.remove("hide");
-        profileNavBtn.classList.add("active-nav-btn");
-    }
-}
+
 
 
---
+++ frontend/scripts/main_page_variables.js
@@ -0,0 +1,23 @@
+// Variables
+const homeNavBtn = document.getElementById("home-nav-btn");
+const msgsNavBtn = document.getElementById("msgs-nav-btn");
+const profileNavBtn = document.getElementById("profile-nav-btn");
+const homeBurgerNavBtn = document.getElementById("home-burger-nav-btn");
+const msgsBurgerNavBtn = document.getElementById("msgs-burger-nav-btn");
+const profileBurgerNavBtn = document.getElementById("profile-burger-nav-btn");
+const homePage = document.getElementById("home-page");
+const msgsPage = document.getElementById("msgs-page");
+const profilePage = document.getElementById("profile-page");
+
+const burgerMenu = document.getElementById("burger-menu");
+const burgerMenuBtns = document.getElementById("burger-menu-btns");
+
+const editProfilemodal = document.getElementById("edit-profile-modal");
+const editProfileBtn = document.getElementById("edit-profile-btn");
+const closeEditProfileBtn = document.getElementById("close-edit-profile-btn");
+
+
+// Modal related stuff
+if (typeof editProfilemodal.showModal !== 'function') {
+    editProfilemodal.hidden = true;
+}
